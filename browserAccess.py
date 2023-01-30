cc1922edd79363369af3bcabc359f8076abaa856
jinnykhwanji <78576272+jinnykhwanji@users.noreply.github.com>
2023-01-30 16:42:00 -0500

    Update browserAccess.py

--- browserAccess.py
+++ browserAccess.py
@@ -1,4 +1,4 @@
-import webbrowser
+
 from time import localtime, strftime
 
 #Get the time data
@@ -13,12 +13,24 @@ i= int(int(hour)/6)
 time_interval=time_slot[i]
 
 #call the website
-url = "https://ftp.wpc.ncep.noaa.gov/shapefiles/qpf/day1/94q_"+year+month+date+time_interval+".tar"
+import webbrowser
+
+name="94q_"+year+month+date+time_interval+".tar"
+url = "https://ftp.wpc.ncep.noaa.gov/shapefiles/qpf/day1/"+name
 webbrowser.get('chrome').open(url)
 
+
 #open up the zip folder& extract data
+import tarfile
+
+file=tarfile.open('/Users/jinny/Downloads/'+name)
+print(file.getnames())
+file.extractall('/Users/jinny/Desktop/Capstone')
+file.close() #be able to get the shape file now (name 94q3012.shp) (94q+last4 digits)
+
 
 #open up ArcGIS and import the shapefile in the folder
+
 
 
 
